5d1d5b53ea96ffeb7f12b2e778e9117ead25d2d5
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-04-08 23:18:30 +0900

    combination

---
+++ src/combination.kt
@@ -0,0 +1,17 @@
+
+fun main() {
+    val list = listOf(1,2,3)
+
+    comb(list)
+}
+
+fun comb(list: List<Int>){
+    if(list.size == 1) return
+
+    for(i in 0 until list.size - 1){
+        println("${list[0]}, ${list[i+1]}")
+    }
+    comb(list.drop(1))
+    return
+}
+
