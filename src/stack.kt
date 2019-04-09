e3a90d216454b8e0d68d2017a44c272feda2019b
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-04-09 20:39:06 +0900

    stack

---
+++ src/stack.kt
@@ -0,0 +1,39 @@
+fun main(args: Array<String>) {
+
+    val s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.push(2)
+    s.push(1)
+
+    s.pop()
+
+    s.print()
+}
+
+class Stack {
+
+    private val max = 5
+    private val data = Array(max) { -1 }
+    var top = 0
+
+    fun push(n: Int) {
+        if (top >= max) throw RuntimeException()
+        data[top] = n
+        top++
+    }
+
+    fun pop() {
+        if (top == 0) throw RuntimeException()
+        data[top-1] = -1
+        top--
+    }
+
+    fun print(){
+        for(i in data){
+            if(i == -1) break
+            println(i)
+        }
+    }
+}
