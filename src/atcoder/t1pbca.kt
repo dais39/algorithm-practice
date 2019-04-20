0445256165de67b36e60fa9e6e5899c1323454f4
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-04-20 21:52:41 +0900

    add t1pbca

---
+++ src/atcoder/t1pbca.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val xs = readLine()!!.split(" ").map { it.toInt() }
+    val c = xs.last()
+
+    if(xs.max() == c || xs.min() == c){
+        println("No")
+    }else{
+        println("Yes")
+    }
+}
