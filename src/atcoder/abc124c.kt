758115d443752339972f36a7245dd2f1ed5d0da0
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-04-21 09:28:30 +0900

    add comment

--- src/atcoder/abc124c.kt
+++ src/atcoder/abc124c.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     val tiles = readLine()!!
     val l = tiles.length
 
-    // 正解の型を作成する
+    // 正解の型を作成する O(n)
     var b = Array(l){'0'}
     var w = Array(l){'1'}
     for (i in 0 until l){
@@ -19,7 +19,7 @@ fun main(args: Array<String>) {
     var bc = 0
     var wc = 0
 
-    // 入力されたタイルの状態と、正解の状態を比較して、塗り替え回数を計算する
+    // 入力されたタイルの状態と、正解の状態を比較して、塗り替え回数を計算する O(n)
     tiles.forEachIndexed { index, c ->
         if(c != b[index]){
             bc++
