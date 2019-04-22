61ee01c8e57e4aaf7f3291d8aa12550c4817a96d
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-04-22 21:36:52 +0900

    add abc121a

---
+++ src/abc121a.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val (H, W) = readLine()!!.split(" ").map { it.toInt() }
+    val (h, w) = readLine()!!.split(" ").map { it.toInt() }
+
+    println((H-h)*(W-w))
+}
