2924e5a6adcb869b4f5b54d31907138addbff5e2
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-05-12 16:22:17 +0900

    add euclidean algorithm

---
+++ src/pattern/euclidean.kt
@@ -0,0 +1,23 @@
+package pattern
+
+fun main(args: Array<String>) {
+
+    // 初期化
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+
+    val ans = if (a > b) euclidean(a, b) else euclidean(b, a)
+
+    println(ans)
+}
+
+/**
+ * ユークリッドの互除法
+ * 2つの自然数(a>=b)において、aのbによる剰余をrとすると、aとbとの最大公約数はbとrとの最大公約数に等しい
+ */
+fun euclidean(divisor: Int, dividend: Int): Int {
+    val r = divisor % dividend
+    if (r == 0) {
+        return dividend
+    }
+    return euclidean(dividend, r)
+}
