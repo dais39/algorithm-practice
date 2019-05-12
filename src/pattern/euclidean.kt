5d0b9b45f6fe2f0810ff6b6ac6d5adfb1c2d1fc2
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-05-12 16:58:43 +0900

    change to private method

--- src/pattern/euclidean.kt
+++ src/pattern/euclidean.kt
@@ -14,7 +14,7 @@ fun main(args: Array<String>) {
  * ユークリッドの互除法
  * 2つの自然数(a>=b)において、aのbによる剰余をrとすると、aとbとの最大公約数はbとrとの最大公約数に等しい
  */
-fun euclidean(divisor: Int, dividend: Int): Int {
+private fun euclidean(divisor: Int, dividend: Int): Int {
     val r = divisor % dividend
     if (r == 0) {
         return dividend
