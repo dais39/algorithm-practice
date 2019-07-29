0323c9b4b286e026b51dfd7fec04c781849ede03
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-07-29 21:41:33 +0900

    abc114b

---
+++ src/abc114/abc114b.kt
@@ -0,0 +1,14 @@
+package abc114
+
+fun main(args: Array<String>) {
+    val s = readLine()!!.split("").drop(1).dropLast(1)
+
+    var min = Integer.MAX_VALUE
+    for(i in (0 until s.size - 2)){
+        val num = "${s[i]}${s[i+1]}${s[i+2]}".toInt()
+        val diff = Math.abs(753 - num)
+        if(min > diff) min = diff
+    }
+
+    println(min)
+}
