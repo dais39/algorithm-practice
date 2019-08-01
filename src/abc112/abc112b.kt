273bd2e9542e737ca23f10f85a78134142285aee
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-08-01 22:06:05 +0900

    abc112b

---
+++ src/abc112/abc112b.kt
@@ -0,0 +1,10 @@
+package abc112
+
+fun main(args: Array<String>) {
+    val (n, t) = readLine()!!.split(" ").map { it.toInt() }
+    val ct = (1..n).map { readLine()!!.split(" ").map { it.toInt() } }
+
+    val ans = ct.filter { it.last() <= t }.minBy { it.first() }
+
+    ans?.let { println(ans[0]) } ?: println("TLE")
+}
