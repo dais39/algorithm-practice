d37784028f3a23f0ba4f90ddfa5b560cb7703c64
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-08-03 09:12:02 +0900

    add abc129c

--- src/abc129/abc129c.kt
+++ src/abc129/abc129c.kt
@@ -14,19 +14,20 @@ fun main(args: Array<String>) {
         steps[a[i] - 1] = false
     }
 
-    if (!steps[0] && !steps[1]) {
-        println(0)
+    // 最上段が1の場合
+    if(n == 1){
+        println(1)
+        return
+    }
+
+    // 最上段が2の場合
+    if(n == 2){
+        if(!steps[0]) println(1) else println(2)
         return
     }
 
     stepAnswers[0] = if (!steps[0]) 0L else 1L
-    stepAnswers[1] = if (!steps[1]) {
-        0L
-    } else if (!steps[0]) {
-        1L
-    } else {
-        2L
-    }
+    stepAnswers[1] = if (!steps[1]) 0L else 1L + stepAnswers[0]
 
     for (i in 2 until n) {
         if (!steps[i - 1] && !steps[i - 2]) {
@@ -34,10 +35,10 @@ fun main(args: Array<String>) {
             return
         }
         if (!steps[i]) {
-            stepAnswers[i] = 0
+            stepAnswers[i] = 0L
             continue
         }
-        stepAnswers[i] = stepAnswers[i - 1] + stepAnswers[i - 2]
+        stepAnswers[i] = (stepAnswers[i - 1] + stepAnswers[i - 2]) % 1000000007L
     }
 
     println(stepAnswers[n - 1] % 1000000007L)
