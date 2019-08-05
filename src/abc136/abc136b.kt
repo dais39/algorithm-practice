c92f3e262f42efa826fc0f292eb798001b82c905
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-08-05 23:11:08 +0900

    add abc136b

--- src/abc136/abc136b.kt
+++ src/abc136/abc136b.kt
@@ -4,12 +4,11 @@ fun main(args: Array<String>) {
 
     val n = readLine()!!.toInt()
 
-    when {
-        n < 10 -> println(n)
-        n < 100 -> println(9)
-        n < 1000 -> println(n - 90)
-        n < 10000 -> println(909)
-        n < 100000 -> println(n - 9090)
-        else -> println(90909)
+    var ans = 0
+
+    for(i in 1..n){
+        if (i.toString().length % 2 == 1) ans++
     }
+
+    println(ans)
 }
