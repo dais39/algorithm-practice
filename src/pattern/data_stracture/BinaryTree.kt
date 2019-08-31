cb895c4174719a4c4e2fd48b4e3dc3ee714db6c4
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-08-31 19:58:47 +0900

    ２分探索木の実装

--- src/pattern/data_stracture/BinaryTree.kt
+++ src/pattern/data_stracture/BinaryTree.kt
@@ -5,7 +5,45 @@ data class Node<K, V>(
     val value: V,
     val left: Node<K, V>?,
     val right: Node<K, V>?
-)
+) {
+
+    companion object {
+
+        fun <K, V> preorder(node: Node<K, V>?) {
+            if (node == null) return
+            println(node.value)
+            preorder(node.left)
+            preorder(node.right)
+        }
+
+        fun <K, V> inorder(node: Node<K, V>?) {
+            if (node == null) return
+            inorder(node.left)
+            println(node.value)
+            inorder(node.right)
+        }
+
+        fun <K, V> postorder(node: Node<K, V>?) {
+            if (node == null) return
+            postorder(node.left)
+            postorder(node.right)
+            println(node.value)
+        }
+
+        fun <K : Comparable<K>, V> searchNode(key: K, root: Node<K, V>): Node<K, V>? {
+
+            var p: Node<K, V>? = root
+            while (p != null) {
+
+                val cond = compareValues(key, p.key)
+                if (cond == 0) return p
+                p = if (cond < 0) p.left else p.right
+            }
+
+            return null
+        }
+    }
+}
 
 fun main() {
 
@@ -13,38 +51,22 @@ fun main() {
         1 -> 2
         1 -> 3
      */
-    val child1 = Node(2,2, null, null)
-    val child2 = Node(3,3, null, null)
-    val root = Node(1, 1, child1, child2)
+    val left = Node(1, 1, null, null)
+    val right = Node(3, 3, null, null)
+    val root = Node(2, 2, left, right)
 
     println("preorder")
-    preorder(root)
+    Node.preorder(root)
 
     println("inorder")
-    inorder(root)
+    Node.inorder(root)
 
     println("postorder")
-    postorder(root)
+    Node.postorder(root)
 
-}
-
-fun <K, V> preorder(node: Node<K, V>?) {
-    if (node == null) return
-    println(node.value)
-    preorder(node.left)
-    preorder(node.right)
-}
-
-fun <K, V> inorder(node: Node<K, V>?) {
-    if (node == null) return
-    inorder(node.left)
-    println(node.value)
-    inorder(node.right)
-}
-
-fun <K, V> postorder(node: Node<K, V>?) {
-    if (node == null) return
-    postorder(node.left)
-    postorder(node.right)
-    println(node.value)
+    println("searchNode")
+    println(Node.searchNode(1, root))
+    println(Node.searchNode(2, root))
+    println(Node.searchNode(3, root))
+    println(Node.searchNode(4, root))
 }
