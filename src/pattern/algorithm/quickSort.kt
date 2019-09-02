8c24d122bd36aeb749bb52769e66e2346ba3fe83
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-09-02 22:22:32 +0900

    add sort algorithm

---
+++ src/pattern/algorithm/quickSort.kt
@@ -0,0 +1,48 @@
+package pattern.algorithm
+
+fun quickSort(array: Array<Int>): Array<Int> {
+
+    if (array.size == 1) return array
+
+    val pivot = array[0]
+    var left = 0
+    var right = array.size - 1
+
+    while (left < right) {
+
+        while (array[left] < pivot) left++
+        while (array[right] > pivot) right--
+
+        if(left <= right){
+            val temp = array[left]
+            array[left] = array[right]
+            array[right] = temp
+
+            left++
+            right--
+        }
+    }
+
+    val leftList = mutableListOf<Int>()
+    val rightList = mutableListOf<Int>()
+    for (i in 0 until left) {
+        leftList.add(array[i])
+    }
+    for (i in left until array.size) {
+        rightList.add(array[i])
+    }
+
+    println(leftList.joinToString(",", "[", "]"))
+    println(rightList.joinToString(",", "[", "]"))
+
+    return quickSort(leftList.toTypedArray()) + quickSort(rightList.toTypedArray())
+}
+
+fun main() {
+
+    val array = arrayOf(2, 5, 3, 2, 7, 3, 1)
+
+    val sortedArray = quickSort(array)
+
+    println(sortedArray.joinToString(",", "[", "]"))
+}
