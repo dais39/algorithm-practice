ff22af3591e8a0cce6609557ae7eff59a041c3db
Daisaku Suzuki <ds.suzuki217@gmail.com>
2020-01-19 17:21:47 +0900

    add leetcode 83

---
+++ src/leetcode/remove_duplicates_from_sorted_list/main.kt
@@ -0,0 +1,19 @@
+package leetcode.remove_duplicates_from_sorted_list
+
+import kotlin.test.assertEquals
+
+fun main() {
+
+    val first = ListNode(1)
+    val second = ListNode(1)
+    val third = ListNode(2)
+    second.next = third
+    first.next = second
+
+    val solution = Solution()
+    val answer = solution.deleteDuplicates(first)
+
+    assertEquals(1, answer?.`val`)
+    assertEquals(2, answer?.next?.`val`)
+    assertEquals(null, answer?.next?.next)
+}
