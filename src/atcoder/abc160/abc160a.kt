e238d990e4dc18c3751f13cf241f1662b0324f32
daisuzz <daisaku.rugby@gmail.com>
2020-03-29 14:26:58 +0900

    abc160a

---
+++ src/atcoder/abc160/abc160a.kt
@@ -0,0 +1,11 @@
+package atcoder.abc160
+
+fun main(args: Array<String>) {
+
+    val s = readLine()!!.toString()
+    if(s[2] == s[3] && s[4] == s[5]){
+        println("Yes")
+    }else{
+        println("No")
+    }
+}
